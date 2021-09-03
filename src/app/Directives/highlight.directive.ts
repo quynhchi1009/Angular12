be01a328d5d2512ae645e422583539228fa7d731
Quynh Chi Lai <quynhchi1009@yandex.com>
2021-09-03 23:26:27 +0700

    directive Handling user events

--- src/app/Directives/highlight.directive.ts
+++ src/app/Directives/highlight.directive.ts
@@ -1,15 +1,27 @@
 import { Input } from '@angular/core';
-import { Directive,  ElementRef  } from '@angular/core';
-
+import { Directive,  ElementRef, HostListener  } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
+
 export class HighlightDirective {
   @Input() appHighlight = 'blue';
   
   constructor(private el: ElementRef) { 
     console.log('HighLight Directives');
     el.nativeElement.style.color = this.appHighlight;
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+  
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 }
